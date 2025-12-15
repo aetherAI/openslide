0e0637501572983d91a8e7ffded3d0454a6d1fe4
Tsung-Ju Lii <tsungju.lii@aetherai.com>
2025-12-15 16:52:54 +0800

    use debian bookworm instead of almalinux to compile
    Signed-off-by: Tsung-Ju Lii <tsungju.lii@aetherai.com>

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/ImagingDataCommons/libdicom/releases/download/v1.2.0/libdicom-1.2.0.tar.xz -O - | tar xf -
-
 meson setup builddir
 meson compile -C builddir
 meson install -C builddir
